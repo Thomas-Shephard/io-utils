d87c0addc7eb996d7b7531493751ff88d5ee99e1
Thomas Shephard <thomas@thomas-shephard.com>
2024-02-11 18:50:21 +0000

    Added comparison utility methods

---
+++ IOUtils/Utils/ComparisonUtils.cs
@@ -0,0 +1,19 @@
+namespace IOUtils.Utils;
+
+internal static class ComparisonUtils {
+    internal static bool GreaterThan<T>(this T input, T? value) where T : IComparable<T> {
+        return input.CompareTo(value) > 0;
+    }
+
+    internal static bool GreaterThanOrEqual<T>(this T input, T? value) where T : IComparable<T> {
+        return input.CompareTo(value) >= 0;
+    }
+
+    internal static bool LessThan<T>(this T input, T? value) where T : IComparable<T> {
+        return !input.GreaterThanOrEqual(value);
+    }
+
+    internal static bool LessThanOrEqual<T>(this T input, T? value) where T : IComparable<T> {
+        return !input.GreaterThan(value);
+    }
+}
