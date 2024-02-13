14b95a94509c4e3d631e319bad7ddf3c12b5cfbe
Thomas Shephard <thomas@thomas-shephard.com>
2024-02-13 11:33:43 +0000

    Add within range comparison utility method (#3)
    * Add within range comparison utility method
    
    * Remove parameter nullability from comparison util methods
    
    * Add min and max check to within range method
    
    * Add unit tests for within range comparison utility method

--- IOUtils/Utils/ComparisonUtils.cs
+++ IOUtils/Utils/ComparisonUtils.cs
@@ -1,19 +1,25 @@
 namespace IOUtils.Utils;
 
 internal static class ComparisonUtils {
-    internal static bool GreaterThan<T>(this T input, T? value) where T : IComparable<T> {
+    internal static bool GreaterThan<T>(this T input, T value) where T : IComparable<T> {
         return input.CompareTo(value) > 0;
     }
 
-    internal static bool GreaterThanOrEqual<T>(this T input, T? value) where T : IComparable<T> {
+    internal static bool GreaterThanOrEqual<T>(this T input, T value) where T : IComparable<T> {
         return input.CompareTo(value) >= 0;
     }
 
-    internal static bool LessThan<T>(this T input, T? value) where T : IComparable<T> {
+    internal static bool LessThan<T>(this T input, T value) where T : IComparable<T> {
         return !input.GreaterThanOrEqual(value);
     }
 
-    internal static bool LessThanOrEqual<T>(this T input, T? value) where T : IComparable<T> {
+    internal static bool LessThanOrEqual<T>(this T input, T value) where T : IComparable<T> {
         return !input.GreaterThan(value);
+    }
+
+    internal static bool WithinRange<T>(this T input, T min, T max) where T : IComparable<T> {
+        if (min.GreaterThan(max))
+            throw new ArgumentException("The minimum value cannot be greater than the maximum value");
+        return input.LessThanOrEqual(max) && input.GreaterThanOrEqual(min);
     }
 }
