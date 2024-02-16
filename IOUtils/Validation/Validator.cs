38c6c49baf68500102a38505204579ad29352764
Thomas Shephard <thomas@thomas-shephard.com>
2024-02-16 12:37:45 +0000

    Add custom validator and utilities for validation

---
+++ IOUtils/Validation/Validator.cs
@@ -0,0 +1,15 @@
+﻿using IOUtils.Providers;
+
+namespace IOUtils.Validation;
+
+public record Validator<T>(Func<T, bool> ValidationCheck, string ErrorMessage) {
+    public bool Validate(T value, IProvider? provider = null) {
+        provider ??= IProvider.Default;
+
+        if (ValidationCheck(value))
+            return true;
+
+        provider.WriteLine(ErrorMessage);
+        return false;
+    }
+}
