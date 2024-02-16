aed76e50ed2f34823f98f6ca247f69f108d82027
Thomas Shephard <thomas@thomas-shephard.com>
2024-02-16 13:33:46 +0000

    Add text input methods

---
+++ IOUtils/Input/TextInput.cs
@@ -0,0 +1,25 @@
+﻿using IOUtils.Providers;
+using IOUtils.Utils;
+using IOUtils.Validation;
+
+namespace IOUtils.Input;
+
+public static class TextInput {
+    private static readonly Validator<string> NotEmptyValidator = new(value => !string.IsNullOrWhiteSpace(value), "That was not valid, enter some text");
+
+    public static string GetText(string question, Validator<string>[]? validators = null, IProvider? provider = null) {
+        provider ??= IProvider.Default;
+        provider.WriteLine(question);
+
+        string input;
+        do {
+            input = provider.ReadLine() ?? "";
+        } while (!validators.Validate(input, provider));
+
+        return input;
+    }
+
+    public static string GetNonEmptyText(string question, IProvider? provider = null) {
+        return GetText(question, new[] { NotEmptyValidator }, provider);
+    }
+}
