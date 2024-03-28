c0f8d9c796b899614a3b637ecdd072b76f9e4089
Thomas Shephard <thomas@thomas-shephard.com>
2024-03-28 21:33:15 +0000

    Update validator to use predicate

--- IOUtils/Validation/Validator.cs
+++ IOUtils/Validation/Validator.cs
@@ -2,11 +2,11 @@
 
 namespace IOUtils.Validation;
 
-public record Validator<T>(Func<T, bool> ValidationCheck, string ErrorMessage) {
+public record Validator<T>(Predicate<T> Predicate, string ErrorMessage) {
     public bool Validate(T value, IProvider? provider = null) {
         provider ??= IProvider.Default;
 
-        if (ValidationCheck(value))
+        if (Predicate(value))
             return true;
 
         provider.WriteLine(ErrorMessage);
