b4ad9dea5fbf6f830d6e213353ef44896b4b1bf7
Thomas Shephard <thomas@thomas-shephard.com>
2024-04-05 13:26:38 +0100

    Add last input property to provider

--- IOUtils/Providers/IProvider.cs
+++ IOUtils/Providers/IProvider.cs
@@ -2,6 +2,7 @@ namespace IOUtils.Providers;
 
 public interface IProvider {
     public static readonly IProvider Default = new ConsoleProvider();
+    public string? LastInput { get; }
     public string? ReadLine();
     public void WriteLine(string? value);
 }
